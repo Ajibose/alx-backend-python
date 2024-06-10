c998873fb209ec51f5dfdd1d2b951dbfc3964732
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-10 19:46:07 +0100

    Task2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    2-measure_runtime.py
+"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    exec_time = time.perf_counter() - start
+    return exec_time / n
