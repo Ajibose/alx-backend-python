0fcaa5fad1fddc5ae00fef6be56bd52948b5f579
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-11 18:34:08 +0100

    Task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
     1-async_comprehension.py
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Async list comprehension"""
     res = [i async for i in async_generator()]
     return res
