e82f2ec6b9a57915902f9db9b88771eb55e679f6
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-11 18:27:46 +0100

    Task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,7 @@
 """
     1-async_comprehension.py
 """
-async_generator = __import__('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
