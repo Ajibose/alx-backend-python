3974f8e0671b46df1b3eda511e75a8f769350c8f
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-06-11 18:24:11 +0100

    Moved the files in

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    1-async_comprehension.py
+"""
+async_generator = __import__('0-async_generator.py').async_generator
+
+
+async def async_comprehension():
+    """Async list comprehension"""
+    res = [i async for i in async_generator()]
+    return res
