cf79b60b80931ba57e1ec65c301d590b58700af3
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-08 21:07:57 +0100

    Task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,11 +11,11 @@ from parameterized import parameterized
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for access_nested_map"""
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
-        ({"a": {"b": 2}}, ("a",)),
-        ({"a": {"b": 2}}, ("a", "b"))
+        {"nested_map": {"a": 1}, "path": ("a",)},
+        {"nested_map": {"a": {"b": 2}}, "path": ("a",)},
+        {"nested_map": {"a": {"b": 2}}, "path": ("a", "b")}
     ])
-    def test_access_nested_map(self, nested_map, path):
+    def test_access_nested_map(self):
         assertEqual(access_nested_map(nested_map, path))
 
 
