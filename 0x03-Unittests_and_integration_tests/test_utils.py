8ac2e859700f12fe2f0dc579ed791c2d3740f4d7
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-09 11:12:52 +0100

    Task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map function with valid paths"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -25,18 +26,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map function with invalid paths"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """TestCase for get_json"""
+    """TestCase for get_json function"""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
+        """Test case for get_json function."""
         mock_get.return_value.json = MagicMock(return_value=test_payload)
         json_resp = get_json(test_url)
         mock_get.assert_called_once()
