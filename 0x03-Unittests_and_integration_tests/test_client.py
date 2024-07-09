bcbe69352fe70775cd34cbc29689f528e29ba29b
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-09 14:29:49 +0100

    Task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test Suite for client module"""
 from client import GithubOrgClient, get_json
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import unittest
 
 
@@ -19,6 +19,20 @@ class TestGithubOrgClient(unittest.TestCase):
         instance.org
         mocked_func.assert_called_once_with(
                 f"https://api.github.com/orgs/{name}")
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mocked_org):
+        """Test the _public_repos_url property"""
+        payload = {
+            "payload": True,
+            "name": "mock",
+            "repos_url": "https://github.com/Ajibose/alx-backend.git"
+        }
+        mocked_org.return_value = payload
+
+        instance = GithubOrgClient("https://api.github.com/orgs/google")
+        val = instance._public_repos_url
+        self.assertEqual(val, payload["repos_url"])
 
 
 if __name__ == '__main__':
