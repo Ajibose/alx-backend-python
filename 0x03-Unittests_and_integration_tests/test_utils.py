34985ca406968b9b3867fd4e53531ba3c4df0a56
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-09 09:25:50 +0100

    Task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,6 @@
 """
 import unittest
 from utils import access_nested_map
-#from parameterized import parameterized
 from parameterized import parameterized
 
 
@@ -18,6 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == '__main__':
