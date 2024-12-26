b6a172abdaee013e43c4d7b366cccbc399fe9a6c
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-12-26 17:17:27 +0100

    Added user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    role = models.CharField(
+            max_length=20
+            choices=[('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin')],
+            default='guest'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
