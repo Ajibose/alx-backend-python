f9715e7274a4db70bee07d7249107108d2a5412d
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-12-26 17:19:49 +0100

    modified user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,7 +4,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email=models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
 
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     role = models.CharField(
