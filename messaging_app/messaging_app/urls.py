39ea44efd90c5fcc4344289e1d7ae38d8f3ca7ec
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-12-29 19:42:44 +0100

    Added auth urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,3 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.url')),
+    path('api-auth/', include('rest_framework.urls')),
